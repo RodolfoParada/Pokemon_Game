f9d98a943fadb4153e2a74416d8afd50c56fbd04
Rodolfo <73254069+RodolfoParada@users.noreply.github.com>
2024-12-03 23:33:28 -0300

    Update pokemonApi.js

--- src/api/pokemonApi.js
+++ src/api/pokemonApi.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const pokemonApi = axios.create({
-    baseURL: 'http://pokeapi.co/api/v2/pokemon'
+    baseURL: 'http://pokeapi.co/api/v2/pokemon';
 })
 
 export default pokemonApi
